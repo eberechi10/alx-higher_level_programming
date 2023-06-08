9f85113e8de7aa43ff12ab778df4b9e499dbd1a8
eberechi10 <eberechukwunemeremjohnsunday@gmail.com>
2023-06-08 07:50:40 -0700

    4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 
-     import hidden_4 *
+     import hidden_4
 if __name__ == "__main__":
-    
-    names = dir()
-    for i in range(len(names)):
-        if names[i][:2] != '__':
-            print("{}".format(names[i]))
+
+    for i in range(hidden_4):
+        if i[:2] != '__':
+            print("{i}".format(i))
