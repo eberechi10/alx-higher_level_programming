9ca25477cc24d7120ca461f0e17e328f4251b3d7
eberechi10 <eberechukwunemeremjohnsunday@gmail.com>
2023-06-26 14:31:02 -0700

    a function that prints x elements of a list

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    sum = 0
+    for p in range(x):
+        try:
+            print(my_list[p], end="")
+            sum += 1
+        except IndexError:
+            break
+    print("")
+    return(sum)
