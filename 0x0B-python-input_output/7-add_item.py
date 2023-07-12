3bf449bb9ad6a9c353090caf13fd03135a2af538
eberechi10 <eberechukwunemeremjohnsunday@gmail.com>
2023-07-11 19:31:54 -0700

    updated

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -9,12 +9,13 @@ save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 if __name__ == "__main__":
+    filename = "add_item.json"
     args = list(sys.argv[1:])
 
-    if os.path.isfile("add_item.json"):
-        my_file = list(load_from_json_file("add_item.json"))
+    if os.path.isfile(filename):
+        my_file = list(load_from_json_file(filename))
     else:
         my_file = []
 
     my_file.extend(args)
-    save_to_json_file(my_file, "add_item.json")
+    save_to_json_file(my_file, filename)
