0fb7a43bc7e3390de13b5406d0926061fae1a874
eberechi10 <eberechukwunemeremjohnsunday@gmail.com>
2023-07-12 02:15:25 -0700

    updated

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -12,25 +12,22 @@ s_code = {"200": 0, "301": 0, "400": 0, "401": 0,
 x = 0
 try:
     for line in sys.stdin:
-    tokens = line.split()
-
-        if len(tokens) >= 2:
+        nod = line.split()
+        if len(nod) >= 2:
             a = x
-            if tokens[-2] in s_code:
-                s_code[tokens[-2]] += 1
+            if nod[-2] in s_code:
+                s_code[nod[-2]] += 1
                 x += 1
             try:
-                f_size += int(tokens[-1])
+                f_size += int(nod[-1])
                 if a == x:
                     x += 1
             except FileNotFoundError:
                 if a == x:
                     continue
-
         if x % 10 == 0:
             print("File size: {:d}".format(f_size))
             for key, value in sorted(s_code.items()):
-
                 if value:
                     print("{:s}: {:d}".format(key, value))
     print("File size: {:d}".format(f_size))
